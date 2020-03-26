f9cc3b143092f34750b3359e281c3bebda4e8be7
Theis Hauge <s154438@student.dtu.dk>
2020-03-26 12:36:35 +0100

    Update GreaterThanOrEqual.java

--- tests/pass/GreaterThanOrEqual.java
+++ tests/pass/GreaterThanOrEqual.java
@@ -1,10 +1,4 @@
 package pass; 
-<<<<<<< HEAD
-=======
-=======
-package pass;
->>>>>>> refs/remotes/origin/master
->>>>>>> branch 'DoubleBranch' of https://github.com/sasp1/compiler-construction.git
 
 public class GreaterThanOrEqual {
 	public boolean greaterThanOrEqual(int x, int y) {
